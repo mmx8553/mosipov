43d9ec27cb01f8d792a2f668764a9702c4418522
Maxim Osipov <msosipov@gmail.com>
2016-12-17 15:58:14 +0300

    cobertura

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,15 +1,39 @@
 package ru.job4j;
 /**
- * Класс вывода мира
+ * Класс вывода мира.
  * @author mosipov
  * @since 06.12.2016
  * @version 1
  *
  */
 public class Calculate {
-
+	/**
+	 * вывода мира.
+	 * @author mosipov
+	 * @since 06.12.2016
+	 *
+	 * @param arg arg **param**
+	 */
 	public static void main(String[] arg) {
 		System.out.println("Hello world");
 	}
-
+	/**
+	 * вывода мира.
+	 * @author mosipov
+	 * @since 06.12.2016
+	 * @return  int int
+	 */
+	public static int add() {
+		return 0;
+	}
+	/**
+	 * вывода мира.
+	 * @author mosipov
+	 * @since 06.12.2016
+	 *
+	 * @return  double double
+	 */
+	public double div() {
+		return 0;
+	}
 }
--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -7,7 +7,7 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
- * Класс для тестирования
+ * Класс для тестирования.
  * @author mosipov
  * @since 15.12.2016
  * @version 1
@@ -15,12 +15,27 @@ import static org.junit.Assert.assertThat;
  */
 public class CalculateTest {
 
-	@Test
+	 /**
+     * Test of printout.
+     */
+
+	 @Test
 	public void whenExecuteMainThenPrintToConsole() {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		System.setOut(new PrintStream(out));
 		Calculate.main(null);
 		assertThat(out.toString(), is("Hello world\r\n"));
+	}
+
+	/**
+     * Test of add.
+     */
+
+	 @Test
+	public void whenExecuteAddThenReturnZero() {
+		Calculate calc = new Calculate();
+		int intAdd = calc.add();
+		assertThat(intAdd, is(0));
 	}
 
 }
